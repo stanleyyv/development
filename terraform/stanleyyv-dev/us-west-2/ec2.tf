1daf27322f49836d164dac575f6e5e903734c1ef
stanleyyv <stanleyyv@gmail.com>
2022-09-04 20:00:16 -0500

    wrapped sg id in brackets]

--- terraform/stanleyyv-dev/us-west-2/ec2.tf
+++ terraform/stanleyyv-dev/us-west-2/ec2.tf
@@ -20,9 +20,9 @@ resource "aws_security_group" "web-sg" {
 
 # instance is in region us-west-2
 resource "aws_instance" "stanley-amazon-linux-1" {
-  ami = "ami-0c2ab3b8efb09f272"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = aws_security_group.web-sg.id
+  ami                    = "ami-0c2ab3b8efb09f272"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   tags = {
     Name = "EC2InstanceByTerraform"
